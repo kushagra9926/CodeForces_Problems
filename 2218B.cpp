eb7f6c7ee2c70525fadbc4304d6b1e06e966a7ea
kushagra9926 <joshikushagra899@gmail.com>
2026-04-27 21:48:16 +0530

    Fix MaxArrayValue initialization and loop condition; refactor main logic for clarity

--- 2218B.cpp
+++ 2218B.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int MaxArrayValue(int *arr, int n){
-    int MaxValue = 0;
+    int MaxValue = arr[0];
     int idx = 0;
-    for(int i = 0; i <= n; i++){
+    for(int i = 0; i < n; i++){
         if(MaxValue < arr[i]){
             MaxValue = arr[i];
             idx = i;
@@ -17,20 +17,20 @@ int main(){
     int n;
     cin >> n;
     while(n--){
-    int arr[7];
-    int sum = 0;
+        int arr[7];
+        int sum = 0;
 
-    for(int i = 0; i < 7; i++){
-        cin >> arr[i];
-    }
-    for(int i = 0; i < 7; i++){
-        if(i == MaxArrayValue(arr, 7)){
-            sum += arr[i];
+        for(int i = 0; i < 7; i++){
+            cin >> arr[i];
         }
-        else{
-            sum += arr[i] * (-1);
+
+        int idx = MaxArrayValue(arr, 7);
+
+        for(int i = 0; i < 7; i++){
+            if(i == idx) sum += arr[i];
+            else sum -= arr[i];
         }
-    }
+
         cout << sum << endl;
     }
     return 0;
