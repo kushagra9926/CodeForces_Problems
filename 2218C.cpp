ff8648d069d17ff77abd920a08b3be1913206a16
kushagra9926 <joshikushagra899@gmail.com>
2026-04-28 14:37:04 +0530

    Refactor 2218C.cpp to uncomment input handling and improve code clarity

--- 2218C.cpp
+++ 2218C.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    // int t;
+    // cin >> t;
+
+    int n;
+    cin >> n;
+        
+    int arr[3*n];
+    for(int i = 0; i < 3 * n; i++){
+        arr[i] = i + 1;
+    }
+    for(int i = 0; i < 3 * n; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
